5ffa35d1ab8e82c509fadebab29a786b3aa770b2
Igor Borozdov <melnikow_igor@mail.ru>
2020-04-28 17:25:48 +0600

    Code refactoring + using class

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,16 +2,18 @@ import React from 'react';
 import logo from './../../logo.svg';
 import './Header.css';
 
-function Header() {
-  return (
-    <div className="Header">
-        <div className="Header-left"></div>
-        <div className="Header-center Logo">
-            <img src={logo} className="App-logo" alt="logo" />
-        </div>   
-        <div className="Header-rigth"></div>
-    </div>
-  );
-}
+export default class Header extends React.Component {
+  
+  render () {
 
-export default Header;
+    return (
+      <div className="Header">
+          <div className="Header-left"></div>
+          <div className="Header-center Logo">
+              <img src={logo} className="App-logo" alt="logo" />
+          </div>   
+          <div className="Header-rigth"></div>
+      </div>
+    );
+  }
+}
